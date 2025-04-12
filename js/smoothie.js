1b4a1ebda58df8d9223f30aa67849b2dfb2b54ca
Mirka <200627729@student.georgianc.on.ca>
2025-04-11 23:09:10 -0400

    Made a class for the  order and made constructor.

---
+++ js/smoothie.js
@@ -0,0 +1,15 @@
+// Make a class for order
+class order {
+
+    //specify attributes
+    contact;
+    size;
+    flavour;
+
+    // make constructor
+    constructor(contact, size, flavour) {
+        this.contact = contact;
+        this.size = size;
+        this.flavour = flavour;
+    }
+}
